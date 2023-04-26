f9a62b500e4a7e06c4af632034992862fc864d1a
esteboulmg <esteban.martingarcia@edu.ece.fr>
2023-04-26 14:15:49 +0200

    premier pas

---
+++ omnes_my_skills.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>OMNES MySkills</title>
+</head>
+<body>
+    <h1>OMNES MySkills</h1>
+    <form action="" method="post">
+        <label for="id">Identifiant</label>
+        <input type="text" name="login" id="login"> <br> <br>
+        <label for="id">Mot de passe</label>
+        <input type="password" name="password" id="password">
+    </form>
+</body>
+</html>
