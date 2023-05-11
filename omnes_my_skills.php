a54c1c5fb79c6cdad8dccbc3872a6aaa114e1fdc
Matthieu Gravejat <matthieu.gravejat@edu.ece.fr>
2023-05-11 14:49:21 +0200

    connexion php

--- omnes_my_skills.php
+++ omnes_my_skills.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -16,3 +16,53 @@
     </form>
 </body>
 </html>
+
+-->
+
+<?php 
+
+$database = "Connexion";
+$db_handle = mysqli_connect('localhost','root',''); 
+$db_found = mysqli_select_db($db_handle, $database);
+
+$username = mysqli_real_escape_string($conn, $_POST['username']);
+$password = mysqli_real_escape_string($conn, $_POST['password']);
+$sql = "SELECT * FROM users WHERE username = '$username' AND password = '$password'";
+$result = mysqli_query($conn, $sql);
+
+
+if (mysqli_num_rows($result) == 1) {
+  session_start();
+  $_SESSION['username'] = $username;
+  header("Location: index.php");
+} else {
+  echo "Nom d'utilisateur ou mot de passe incorrect.";
+}
+
+mysqli_close($conn);
+?>
+
+<?php
+// Démarrer la session
+session_start();
+
+// Vérifier si l'utilisateur est connecté
+if (!isset($_SESSION['username'])) {
+  header("Location: login.php");
+}
+
+// Afficher le nom d'utilisateur de l'utilisateur connecté
+echo "Bonjour, " . $_SESSION['username'] . "!";
+
+?>
+
+
+
+
+
+
+
+
+
+
+?>
