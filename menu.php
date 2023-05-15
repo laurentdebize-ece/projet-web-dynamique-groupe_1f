9ca7846e08361666b04c0b57da678e4f6fb9d8e4
Matthieu Gravejat <matthieu.gravejat@edu.ece.fr>
2023-05-15 11:22:14 +0200

    test connexion

--- menu.php
+++ menu.php
@@ -1,36 +1,9 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Formulaire d'inscription</title>
+	<title>Connexion</title>
 </head>
 <body>
-	<form method="post" action="premiere_connexion.php">
-		<label for="nom">Nom :</label>
-		<input type="text" id="nom" name="lastname"><br>
-
-		<label for="prenom">Prénom :</label>
-		<input type="text" id="prenom" name="firstname"><br>
-
-		<label for="email">Email :</label>
-		<input type="email" id="email" name="email"><br>
-
-		<label for="classe">Classe :</label>
-		<select id="classe" name="class">
-			<option value="etudiant">Etudiant</option>
-			<option value="scolarite">Professeur</option>
-			<option value="administrateur">Administrateur</option>
-		</select><br>
-
-		<label for="code">Code :</label>
-		<input type="text" id="code" name="code"><br>
-
-		<label for="mdp">Mot de passe :</label>
-		<input type="password" id="mdp" name="passwordReg"><br>
-
-		<label for="mdp_confirm">Confirmation du mot de passe :</label>
-		<input type="password" id="mdp_confirm" name="passwordConfirm"><br>
-
-		<input type="submit" value="Inscription">
-	</form>
+	<h1> Bienvenue </h1>
 </body>
 </html>
--- omnes_my_skills.php
+++ omnes_my_skills.php
@@ -10,9 +10,9 @@
     <h1>OMNES MySkills</h1>
     <form action="" method="post">
         <label for="login">Identifiant</label>
-        <input type="text" name="login" id="login"> <br> <br>
+        <input type="text" name="Identifiant" id="login"> <br> <br>
         <label for="password">Mot de passe</label>
-        <input type="password" name="password" id="password">
+        <input type="password" name="Motdepasse" id="password">
         <br><br>
         <input type="submit" value="Se connecter">
     </form>
@@ -20,19 +20,23 @@
     <?php 
     session_start();
 
-    if (isset($_POST['login']) && isset($_POST['password'])) {
-        $database = "omnesmyskills";
+    if (isset($_POST['Identifiant']) && isset($_POST['Motdepasse'])) {
+        $database = "omnesmyskills.sql";
         $db_handle = mysqli_connect('localhost','root','Ficelle2003!'); 
         $db_found = mysqli_select_db($db_handle, $database);
+        
+        if (!$db_found) {
+          die("Database not found.");
+      }
 
-        $username = mysqli_real_escape_string($db_handle, $_POST['login']);
-        $password = mysqli_real_escape_string($db_handle, $_POST['password']);
-        $sql = "SELECT * FROM connexion WHERE Identifiant = '$username' AND Motdepasse = '$password'";
+        $username = mysqli_real_escape_string($db_handle, $_POST['Identifiant']);
+        $password = mysqli_real_escape_string($db_handle, $_POST['Motdepasse']);
+        $sql = "SELECT * FROM connexion WHERE Identifiant = '".$username."' AND Motdepasse = '".$password."'";
         $result = mysqli_query($db_handle, $sql);
 
         if ($result) {
             $_SESSION['Identifiant'] = $username;
-            echo "Bienvenue".$username;
+            echo "Bienvenue ".$username;
             header("Location: menu.php");
             exit();
         } else {
