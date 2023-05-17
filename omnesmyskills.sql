cda1e008f1c8cf13b863cf6f4b0d49b3a06c1b5e
Matthieu Gravejat <matthieu.gravejat@edu.ece.fr>
2023-05-17 12:30:09 +0200

    bdd competence

--- omnesmyskills.sql
+++ omnesmyskills.sql
@@ -1,31 +1,8 @@
--- phpMyAdmin SQL Dump
--- version 5.1.2
--- https://www.phpmyadmin.net/
---
--- Host: localhost:3306
--- Generation Time: May 17, 2023 at 09:44 AM
--- Server version: 5.7.24
--- PHP Version: 8.0.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `omnesmyskills`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `administrateur`
---
 
 CREATE TABLE `administrateur` (
   `Prenom` varchar(255) NOT NULL,
@@ -34,9 +11,6 @@ CREATE TABLE `administrateur` (
   `Motdepasse` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Dumping data for table `administrateur`
---
 
 INSERT INTO `administrateur` (`Prenom`, `Nom`, `Email`, `Motdepasse`) VALUES
 ('Matthieu', 'Gravejat', 'matthieu.gravejat@edu.ece.fr', 'Admin123'),
@@ -44,6 +18,3 @@ INSERT INTO `administrateur` (`Prenom`, `Nom`, `Email`, `Motdepasse`) VALUES
 ('Leopold', 'Thierry', 'leopold.thierry@edu.ece.fr', 'Admin123');
 COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
