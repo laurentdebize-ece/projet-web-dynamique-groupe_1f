3984a11127afc81c6827c071caf1b3c0d9680a3f
esteboulmg <esteban.martingarcia@edu.ece.fr>
2023-05-22 16:01:56 +0200

    nouvelles avancées

--- action_connexion_esteban.php
+++ action_connexion_esteban.php
@@ -1,4 +1,5 @@
 <?php
+header("no-cache, no-store, must-revalidate");
 try
 {
    $bdd = new PDO('mysql:host=localhost;dbname=BDMYSKILLS;
@@ -10,22 +11,59 @@ catch (Exception $e)
    die('Erreur :'.$e->getMessage());
 }
 
-$reqtot = $bdd->query("SELECT * FROM Etudiant");
-$reqemail = $bdd->query("SELECT Email FROM Etudiant");
-$reqpass = $bdd->query("SELECT MotDePasse FROM Etudiant");
-
 echo '<br>';
 
-if(isset($_POST["login"],$_POST["password"]) && !empty($_POST["login"] && !empty($_POST["password"]))){
-    while ($row = $reqtot->fetch()) {
-        echo "<tr><td>" . $row['Email'] . "</td>    <td>" . $row['MotDePasse'] . "</td></tr> <br>";
-        if(htmlspecialchars($_POST["login"]) == $row['Email'] 
-        && htmlspecialchars($_POST["password"]) == $row['MotDePasse']){
-            echo 'successed <br>';
+switch ($_POST["selection"]) {
+    case 1:
+        $reqtot = $bdd->query("SELECT * FROM Administrateur");
+        $reqemail = $bdd->query("SELECT Email FROM Administrateur");
+        $reqpass = $bdd->query("SELECT MotDePasse FROM Administrateur");
+        if(isset($_POST["login"],$_POST["password"]) && !empty($_POST["login"] && !empty($_POST["password"]))){
+            while ($row = $reqtot->fetch()) {
+                echo "<tr><td>" . $row['Email'] . "</td>    <td>" . $row['MotDePasse'] . "</td></tr> <br>";
+                if(htmlspecialchars($_POST["login"]) == $row['Email'] 
+                && htmlspecialchars($_POST["password"]) == $row['MotDePasse']){
+                    echo 'successed <br>';
+                }
+                else {
+                    echo 'failed <br>';
+                }
+            }
         }
-        else {
-            echo 'failed <br>';
+        break;
+    case 2:
+        $reqtot = $bdd->query("SELECT * FROM Professeur");
+        $reqemail = $bdd->query("SELECT Email FROM Professeur");
+        $reqpass = $bdd->query("SELECT MotDePasse FROM Professeur");
+        if(isset($_POST["login"],$_POST["password"]) && !empty($_POST["login"] && !empty($_POST["password"]))){
+            while ($row = $reqtot->fetch()) {
+                echo "<tr><td>" . $row['Email'] . "</td>    <td>" . $row['MotDePasse'] . "</td></tr> <br>";
+                if(htmlspecialchars($_POST["login"]) == $row['Email'] 
+                && htmlspecialchars($_POST["password"]) == $row['MotDePasse']){
+                    echo 'successed <br>';
+                }
+                else {
+                    echo 'failed <br>';
+                }
+            }
         }
-    }
+        break;
+    case 3:
+        $reqtot = $bdd->query("SELECT * FROM Etudiant");
+        $reqemail = $bdd->query("SELECT Email FROM Etudiant");
+        $reqpass = $bdd->query("SELECT MotDePasse FROM Etudiant");
+        if(isset($_POST["login"],$_POST["password"]) && !empty($_POST["login"] && !empty($_POST["password"]))){
+            while ($row = $reqtot->fetch()) {
+                echo "<tr><td>" . $row['Email'] . "</td>    <td>" . $row['MotDePasse'] . "</td></tr> <br>";
+                if(htmlspecialchars($_POST["login"]) == $row['Email'] 
+                && htmlspecialchars($_POST["password"]) == $row['MotDePasse']){
+                    echo 'successed <br>';
+                }
+                else {
+                    echo 'failed <br>';
+                }
+            }
+        }
+        break;
 }
 ?>
--- omnes_my_skills.php
+++ omnes_my_skills.php
@@ -10,6 +10,12 @@
 <body>
     <h1>OMNES MySkills</h1>
     <form action="action_connexion_esteban.php" method="post">
+        <select name="selection">
+            <option value="0">--Veuillez sélectionner une option--</option>
+            <option value="1" name="1">Administrateur</option>
+            <option value="2" name="2">Professeur</option>
+            <option value="3" name="3">Étudiant</option>
+        </select> <br>
         <label for="id">Identifiant</label>
         <input type="text" name="login" id="login"> <br> <br>
         <label for="id">Mot de passe</label>
